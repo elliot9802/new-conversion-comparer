76b186fd2d68529466d268c00547bb8cfb23da34
elliot9802 <elliot@segerlind.se>
2023-10-23 13:14:04 +0200

    Added logic for EvoPdf with XML comments

--- Services/EvoPdfService.cs
+++ Services/EvoPdfService.cs
@@ -1,27 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EvoPdf;
+using Syncfusion.Pdf;
 
 namespace Services
 {
+    /// <summary>
+    /// Service class responsible for converting HTML content and URLs to PDF format.
+    /// This implementation leverages the ExpertPdf library, specifically using its PdfConverter class.
+    /// The PdfConverter is initialized with predefined settings suitable for the PDF conversion tasks.
+    /// </summary>
     public class EvoPdfService : IUtilityService
     {
         private readonly IFileService _fileService;
+        private readonly PdfConverter _pdfConverter;
 
         public EvoPdfService(IFileService fileService)
         {
             _fileService = fileService;
+            _pdfConverter = CreatePdfConverter();
         }
         public void ConvertHtmlToPdf(string htmlContent, string outputPath)
         {
-            throw new NotImplementedException();
+            byte[] pdfBytes = _pdfConverter.GetPdfBytesFromHtmlString(htmlContent);
+            _fileService.WriteAllBytes(outputPath, pdfBytes);
         }
 
         public void ConvertUrlToPdf(string urlContent, string outputPath)
         {
-            throw new NotImplementedException();
+            byte[] pdfBytes = _pdfConverter.GetPdfBytesFromUrl(urlContent);
+            _fileService.WriteAllBytes(outputPath, pdfBytes);
+        }
+
+        /// <summary>
+        /// Creates an instance of PdfConverter with predefined settings.
+        /// </summary>
+        /// <returns>Configured instance of PdfConverter.</returns>
+        private PdfConverter CreatePdfConverter()
+        {
+            PdfConverter pdfConverter = new PdfConverter
+            {
+                PdfDocumentOptions =
+                {
+                    PdfPageSize = PdfPageSize.A4,
+                    PdfCompressionLevel = PdfCompressionLevel.Normal,
+                    ShowHeader = false,
+                    ShowFooter = false
+                }
+            };
+
+            return pdfConverter;
         }
     }
 }
