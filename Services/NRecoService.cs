9539cd5669289b28bc18a0b84bbfa218bee42ddd
elliot9802 <elliot@segerlind.se>
2023-10-23 13:16:34 +0200

    Added logic for NReco

--- Services/NRecoService.cs
+++ Services/NRecoService.cs
@@ -1,27 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Aspose.Pdf;
+using NReco.PdfGenerator;
 
 namespace Services
 {
+    /// <summary>
+    /// Service class responsible for converting HTML content and URLs to PDF format.
+    /// This implementation leverages the ExpertPdf library, specifically using its PdfConverter class.
+    /// The PdfConverter is initialized with predefined settings suitable for the PDF conversion tasks.
+    /// </summary>
     public class NRecoService : IUtilityService
     {
         private readonly IFileService _fileService;
+        private readonly HtmlToPdfConverter _pdfConverter;
+
         public NRecoService(IFileService fileService)
         {
-            _fileService = fileService; 
+            _fileService = fileService;
+            _pdfConverter = CreatePdfConverter();
         }
-
         public void ConvertHtmlToPdf(string htmlContent, string outputPath)
         {
-            throw new NotImplementedException();
+            byte[] pdfBytes = _pdfConverter.GeneratePdf(htmlContent);
+            _fileService.WriteAllBytes(outputPath, pdfBytes);
         }
 
         public void ConvertUrlToPdf(string urlContent, string outputPath)
         {
-            throw new NotImplementedException();
+            _pdfConverter.GeneratePdfFromFile(urlContent, null, outputPath);
+            // The PDF is already written to outputPath, no need to write again
+        }
+
+        /// <summary>
+        /// Creates an instance of PdfConverter with predefined settings.
+        /// </summary>
+        /// <returns>Configured instance of PdfConverter.</returns>
+        private HtmlToPdfConverter CreatePdfConverter()
+        {
+            HtmlToPdfConverter pdfConverter = new HtmlToPdfConverter
+            {
+                Orientation = PageOrientation.Portrait,
+                Size = PageSize.A4
+            };
+
+            return pdfConverter;
         }
     }
 }
